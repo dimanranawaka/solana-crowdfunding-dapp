b6201394b4b863739d2856af9a03479ec4614f11
dimanranawaka <dimanranawaka@gmail.com>
2024-08-12 13:57:57 +0530

    implement context for the create function

--- programs/crowdfundingdapp/src/lib.rs
+++ programs/crowdfundingdapp/src/lib.rs
@@ -15,5 +15,17 @@ pub mod crowdfundingdapp {
     campaign.admin = *ctx.accounts.user.key;
     Ok(())
    }
+
+   #[derive(Accounts)]
+   pub struct Create<'info>{
+
+    #[account(init,payer=user,space=9000, seeds=[b"CAMPAIGN_DEMO".as_ref(),user.key().as_ref()],bump)]
+    pub campaign: Account<'info,Campaign>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info,System>
+
+   }
 }
 
