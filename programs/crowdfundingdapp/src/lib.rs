c4a09fda7fe95302531ae1939be9093ab157e45a
dimanranawaka <dimanranawaka@gmail.com>
2024-08-12 11:01:04 +0530

    implement create function

--- programs/crowdfundingdapp/src/lib.rs
+++ programs/crowdfundingdapp/src/lib.rs
@@ -6,5 +6,14 @@ declare_id!("5KJrdYJAaTaxu4WFcktxp7wjCfLGa4xujvFSCK7HLpDW");
 pub mod crowdfundingdapp {
     use super::*;
 
+   pub fn create(ctx:Context<Create>, name:String,description:String)->Result<()>{
+
+    let campaign = &mut ctx.accounts.campaign;
+    campaign.name = name;
+    campaign.description = description;
+    campaign.amount_donated = 0;
+    campaign.admin = *ctx.accounts.user.key;
+    Ok(())
+   }
 }
 
